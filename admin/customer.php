bf119c2bccaea6f2accfa5eb1c1b3d55a86c791d
shoyab <abumansurshoyab@gmail.com>
2023-06-29 17:54:26 +0600

    Modification in customer.php file

--- admin/customer.php
+++ admin/customer.php
@@ -1,4 +1,5 @@
 <?php include ("./header.php"); ?>
+<?php require ("./src/database.php"); ?>
 
     <div class="content-wrapper">
         <!-- START PAGE CONTENT-->
@@ -11,6 +12,23 @@
                <!-- <li class="breadcrumb-item"></li>-->
             </ol>
         </div>
+        <?php
+            if (isset($_POST['submit'])){
+                $pname = $_POST['pname'];
+                $ptype = $_POST['ptype'];
+                $phone = $_POST['phone'];
+                $email = $_POST['email'];
+                $address = $_POST['address'];
+                $conpersone = $_POST['conpersone'];
+                $cpphone = $_POST['cpphone'];
+
+                
+                $insert = "INSERT INTO `customer`(`party_name`, `party_type`, `address_ar`, `phone`, `email`, `contact_person`, `cp_phone`) VALUES ('{$pname}','{$ptype}','{$phone}','{$email}','{$address}','{$conpersone}','{$cpphone}')";
+                $con->query($insert);
+            };
+
+            
+        ?>
 
         <!--    Enter Your Content Here-->
         <div class="page-content fade-in-up">
@@ -26,6 +44,7 @@
         </div>
         <!-- modal button end -->
 
+ 
         <!-- modal start -->
         <div class="modal fade" id="DonorAdd" tabindex="-1" role="dialog" aria-labelledby="DonorAdd" aria-hidden="true">
                               <form action="" method="POST">
@@ -45,12 +64,12 @@
 										  
                                     <div class="row">
                                         <div class="col-sm-6 form-group">
-                                            <label>Customer Name : </label>
-                                            <input name="reqfrom" class="form-control" type="text" placeholder="Full Name">
+                                            <label>Party Name : </label>
+                                            <input name="pname" class="form-control" type="text" placeholder="Full Name" required>
                                         </div>
                                         <div class="col-sm-6 form-group">
                                             <label>Party Type : </label>
-                                            <input name="pname" class="form-control" type="text" placeholder="Party Type">
+                                            <input name="ptype" class="form-control" type="text" placeholder="Party Type">
                                         </div>
                                     </div>
                                     <div class="row">
@@ -86,13 +105,68 @@
                                           </div>
                                           <div class="modal-footer">
 <!--                                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>-->
-                                              <button class="btn btn-default px-4" type="submit">Save</button>
+                                              <button name="submit" class="btn btn-default px-4" type="submit">Save</button>
                                           </div>
                                       </div>
                                   </div>
                               </form>
                           </div>
         <!-- modal end -->
+        <?php 
+            $select = 'SELECT * FROM customer';
+            $query = $con->query($select);
+            $result = $query->fetch_all(MYSQLI_ASSOC);
+        ?>
+
+
+
+
+                    <div class="ibox-body">
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+
+                                <tr>
+                                    <th width="50px">SL</th>
+                                    <th>Party Name</th>
+                                    <th>Party Type</th>
+                                    <th>Address</th>
+                                    <th>Phone</th>
+                                    <th>Email</th>
+                                    <th>Contact Person</th>
+                                    <th>Contact Person Phone</th>
+                                    <th>Acct Head</th>
+                                    <th>Head Type</th>
+                                    <th>Create At</th>
+                                    <th>Update At</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+
+                                <?php foreach($result as $value): ?>
+                            <tr>
+                                <td><?= $value['id']; ?></td>
+                                <td><?= $value['party_name']; ?></td>
+                                <td><?= $value['party_type']; ?></td>
+                                <td><?= $value['address_ar']; ?></td>
+                                <td><?= $value['phone']; ?></td>
+                                <td><?= $value['email']; ?></td>
+                                <td><?= $value['contact_person']; ?></td>
+                                <td><?= $value['cp_phone']; ?></td>
+                                <td><?= $value['acct_head']; ?></td>
+                                <td><?= $value['head_type']; ?></td>
+                                <td><?= $value['create_at']; ?></td>
+                                <td><?= $value['updated_at']; ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+
+            
+                                </tbody>
+                            </table>
+                        </div>
+
+
+
                     </div>        
                 </div>
             </div>

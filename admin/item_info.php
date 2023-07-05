fd842b735e7a2cba9b20b4b94a2fc462a2a692ea
Mojahid Rafi <mojahid.rafi17@gmail.com>
2023-07-06 03:17:03 +0600

    Select 2 Plugin Added For Select Option (In Item Info Form)

--- admin/item_info.php
+++ admin/item_info.php
@@ -51,63 +51,66 @@
 
         <!-- modal start -->
         <div class="modal fade" id="DonorAdd" tabindex="-1" role="dialog" aria-labelledby="DonorAdd" aria-hidden="true">
-                              <form action="" method="POST">
-                                  <div class="modal-dialog modal-lg" role="document">
-                                      <div class="modal-content">
-                                          <div class="modal-header">
-                                              <h5 class="modal-title" id="exampleModalLabel">Item</h5>
-                                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                  <span aria-hidden="true">&times;</span>
-                                              </button>
-                                          </div>
-                                          <div class="modal-body">
-										  
-										  <!--form body start here-->
-										  
-										  
-										  
-                                    <div class="row">
-                                        <div class="col-sm-6 form-group">
-                                            <label>Item Code : </label>
-                                            <input name="icode" class="form-control" type="text" placeholder="Item Code">
-                                        </div>
-                                        <div class="col-sm-6 form-group">
-                                            <label>Catagory Id : </label>
-                                            <input name="catid" class="form-control" type="text" placeholder="Catagory Id">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-6 form-group">
-                                            <label>Unit : </label>
-                                            <input name="unit" class="form-control" type="text" placeholder="Unit">
-                                        </div>
-                                        <div class="col-sm-6 form-group">
-                                            <label>Unit Price :</label>
-                                            <input name="uprice" class="form-control" type="text" placeholder="Unit Price">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-6 form-group">
-                                            <label>Last Per Price :</label>
-                                            <input name="lpprice" class="form-control" type="text" placeholder="Last Per Price">
-                                        </div>
-                                        <div class="col-sm-6 form-group">
-                                            <label>Barcode :</label>
-                                            <input name="barcode" class="form-control" type="text" placeholder="Barcode">
-                                        </div>
-                                    </div>
+            <form action="" method="POST">
+                <div class="modal-dialog modal-lg" role="">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Item</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
 
-											<!--form body end -->
-											
-											
-                                          </div>
-                                          <div class="modal-footer">
-<!--                                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>-->
-                                              <button name="submit" class="btn btn-default px-4" type="submit">Save</button>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </form>
+                            <!--form body start here-->
+                            <div class="row">
+                                <div class="col-sm-6 form-group">
+                                    <label>Item Code : </label>
+                                    <input name="icode" class="form-control" type="text" placeholder="Item Code">
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label class="form-control-label">Select Category</label>
+                                        <select name="catid" class="form-control select2_style1" style="width: 100%;">
+                                            <option value="1">Category 1</option>
+                                            <option value="2">Category 2</option>
+                                            <option value="3">Category 3</option>
+                                            <option value="4">Category 4</option>
+                                            <option value="5">Category 5</option>
+                                            <option value="6">Category 6</option>
+                                            <option value="7">Category 7</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6 form-group">
+                                    <label>Unit : </label>
+                                    <input name="unit" class="form-control" type="text" placeholder="Unit">
+                                </div>
+                                <div class="col-sm-6 form-group">
+                                    <label>Unit Price :</label>
+                                    <input name="uprice" class="form-control" type="text" placeholder="Unit Price">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6 form-group">
+                                    <label>Last Per Price :</label>
+                                    <input name="lpprice" class="form-control" type="text" placeholder="Last Per Price">
+                                </div>
+                                <div class="col-sm-6 form-group">
+                                    <label>Barcode :</label>
+                                    <input name="barcode" class="form-control" type="text" placeholder="Barcode">
+                                </div>
+                            </div>
+                            <!--form body end -->
+                        </div>
+                        <div class="modal-footer">
+                            <button name="submit" class="btn btn-default px-4" type="submit">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
                           </div>
         <!-- modal end -->
         <?php 
@@ -120,6 +123,7 @@
 
 
                     <div class="ibox-body">
+
                         <div class="table-responsive">
                             <table class="table">
                                 <thead>

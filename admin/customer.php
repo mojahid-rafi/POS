03cfb553e0cd076652df4477f5383c469425f691
shoyab <abumansurshoyab@gmail.com>
2023-07-07 20:14:02 +0600

    customers file modified

--- admin/customer.php
+++ admin/customer.php
@@ -14,8 +14,7 @@
         </div>
         <?php
             if (isset($_POST['submit'])){
-                $pname = $_POST['pname'];
-                $ptype = $_POST['ptype'];
+                $pname = $_POST['cname'];
                 $phone = $_POST['phone'];
                 $email = $_POST['email'];
                 $address = $_POST['address'];
@@ -23,32 +22,28 @@
                 $cpphone = $_POST['cpphone'];
 
                 
-                $insert = "INSERT INTO customers (party_name, party_type, address_ar, phone, email, contact_person, cp_phone) VALUES ('{$pname}', '{$ptype}', '{$address}', '{$phone}', '{$email}', '{$conpersone}', '{$cpphone}')";
+                $insert = "INSERT INTO `customers`(`party_name`, `party_type`, `address_ar`, `phone`, `email`, `contact_person`, `cp_phone`,`user_id`) VALUES ('{$pname}', 'customer', '{$address}', '{$phone}', '{$email}', '{$conpersone}', '{$cpphone}', '0')";
                 $con->query($insert);
-
-
             };
-
-            
         ?>
 
-        <!--    Enter Your Content Here-->
+        <!-- Enter Your Content Here-->
         <div class="page-content fade-in-up">
             <div class="row">
                 <div class="col-md-12">
                     <div class="ibox">
-        <!-- modal button -->
-        <div class="ibox-head justify-content-end">
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#DonorAdd">
-                Add Customer
-            </button>
-        </div>
-        <!-- modal button end -->
+                        <!-- modal button -->
+                        <div class="ibox-head justify-content-end">
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#DonorAdd">
+                                Add Customer
+                            </button>
+                        </div>
+                        <!-- modal button end -->
 
- 
-        <!-- modal start -->
-        <div class="modal fade" id="DonorAdd" tabindex="-1" role="dialog" aria-labelledby="DonorAdd" aria-hidden="true">
+                        <!-- modal start -->
+                            <div class="modal fade" id="DonorAdd" tabindex="-1" role="dialog"
+                            aria-labelledby="DonorAdd" aria-hidden="true">
                               <form action="" method="POST">
                                   <div class="modal-dialog modal-lg" role="document">
                                       <div class="modal-content">
@@ -62,85 +57,68 @@
 										  
 										  <!--form body start here-->
 										  
-										  
-										  
-                                    <div class="row">
-                                        <div class="col-sm-6 form-group">
-                                            <label>Party Name : </label>
-                                            <input name="pname" class="form-control" type="text" placeholder="Full Name" required>
-                                        </div>
-                                        <div class="col-sm-6 form-group">
-                                            <label>Party Type : </label>
-                                            <input name="ptype" class="form-control" type="text" placeholder="Party Type">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-6 form-group">
-                                            <label>Phone : </label>
-                                            <input name="phone" class="form-control" type="text" placeholder="Phone Number">
-                                        </div>
-                                        <div class="col-sm-6 form-group">
-                                            <label>Email : </label>
-                                            <input name="email" class="form-control" type="email" placeholder="Email">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-6 form-group">
-                                            <label>Address :</label>
-                                            <textarea name="address" class="form-control" id="address" cols="10" rows="2" placeholder="Address"></textarea>
-                                        </div>
-                                        <div class="col-sm-6 form-group">
-                                            <label>Contact Persone :</label>
-                                            <input name="conpersone" class="form-control" type="text" placeholder="Contact Persone">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-6 form-group">
-                                            <label>Contact Persone Phone :</label>
-                                            <input name="cpphone" class="form-control" type="text" placeholder="Contact Persone Phone">
-                                        </div>
-                                    </div>
-
+                                            <div class="row">
+                                                <div class="col-sm-6 form-group">
+                                                    <label>Customer Name : </label>
+                                                    <input name="cname" class="form-control" type="text" placeholder="Full Name" required>
+                                                </div>
+                                                <div class="col-sm-6 form-group">
+                                                    <label>Phone : </label>
+                                                    <input name="phone" class="form-control" type="text" placeholder="Phone Number">
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-sm-6 form-group">
+                                                    <label>Email : </label>
+                                                    <input name="email" class="form-control" type="email" placeholder="Email">
+                                                </div>
+                                                <div class="col-sm-6 form-group">
+                                                    <label>Address :</label>
+                                                    <textarea name="address" class="form-control" id="address" cols="10" rows="2" placeholder="Address"></textarea>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-sm-6 form-group">
+                                                    <label>Contact Persone :</label>
+                                                    <input name="conpersone" class="form-control" type="text" placeholder="Contact Persone">
+                                                </div>
+                                                <div class="col-sm-6 form-group">
+                                                    <label>Contact Persone Phone :</label>
+                                                    <input name="cpphone" class="form-control" type="text" placeholder="Contact Persone Phone">
+                                                </div>
+                                            </div>
 											<!--form body end -->
-											
 											
                                           </div>
                                           <div class="modal-footer">
-<!--                                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>-->
                                               <button name="submit" class="btn btn-default px-4" type="submit">Save</button>
                                           </div>
                                       </div>
                                   </div>
                               </form>
                           </div>
-        <!-- modal end -->
-        <?php 
-            $select = 'SELECT * FROM customers';
-            $query = $con->query($select);
-            $result = $query->fetch_all(MYSQLI_ASSOC);
-        ?>
-
-
-
-
+                     <!-- modal end -->
+                        <?php 
+                            $select = 'SELECT * FROM customers';
+                            $query = $con->query($select);
+                            $result = $query->fetch_all(MYSQLI_ASSOC);
+                        ?>
                     <div class="ibox-body">
                         <div class="table-responsive">
                             <table class="table">
                                 <thead>
-
-                                <tr>
-                                    <th width="50px">SL</th>
-                                    <th>Party Name</th>
-                                    <th>Address</th>
-                                    <th>Phone</th>
-                                    <th>Email</th>
-                                    <th>Contact Person</th>
-                                    <th>Contact Person Phone</th>
-                                </tr>
+                                    <tr>
+                                        <th width="50px">SL</th>
+                                        <th>Party Name</th>
+                                        <th>Address</th>
+                                        <th>Phone</th>
+                                        <th>Email</th>
+                                        <th>Contact Person</th>
+                                        <th>Contact Person Phone</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-
-                                <?php foreach($result as $value): ?>
+                        <?php foreach($result as $value): ?>
                             <tr>
                                 <td><?= $value['id']; ?></td>
                                 <td><?= $value['party_name']; ?></td>
@@ -150,9 +128,7 @@
                                 <td><?= $value['contact_person']; ?></td>
                                 <td><?= $value['cp_phone']; ?></td>
                                 <td> 
-                                    <a href="editcustomer.php?id=<?= $value['id']; ?>" class="btn btn-default btn-xs m-r-5"><i class="fa fa-pencil font-14"></i></a>
-
-
+                                <a href="editcustomer.php?id=<?= $value['id']; ?>" class="btn btn-default btn-xs m-r-5"><i class="fa fa-pencil font-14"></i></a>
 
                                  <a href="delete_customer.php?id=<?= $value['id']; ?>" class="btn btn-default btn-xs m-r-5"><i class="fa fa-trash font-14"></i></a>
                                 </td>
--- admin/editcustomer.php
+++ admin/editcustomer.php
@@ -16,88 +16,62 @@
 
         <?php
 
- $id = $_GET['id'];
+            $id = $_GET['id'];
 
-
-            
-            $select = "SELECT * FROM customer WHERE id=$id";
+            $select = "SELECT * FROM customers WHERE id=$id";
             $query = $con->query($select);
             while($fetch = mysqli_fetch_assoc($query)){
-                ?>
+        ?>
            
-
-                
-
-
-
-        <form action="updatecustomer.php" method="POST">
+    <form action="updatecustomer.php" method="POST">
         <input type="text" name="id" hidden value="<?php echo $fetch['id'];  ?>" >
-                                  <div class="modal-dialog modal-lg" role="document">
-                                      <div class="modal-content">
-                                          
-                                          <div class="modal-body">
-										  
-										  <!--form body start here-->
-										  
-										  
-										  
-                                    <div class="row">
-                                        <div class="col-sm-6 form-group">
-                                            <label>Party Name : </label>
-                                            
-                                            <input class="form-control" type="text" name="party_name" value="<?php echo $fetch['party_name']; ?> ">
-                                        </div>
-                                        <div class="col-sm-6 form-group">
-                                            <label>Party Type : </label>
+             <div class="modal-dialog modal-lg" role="document">
+                 <div class="modal-content">
+                     <div class="modal-body">
+				  
+				  <!--form body start here-->
+				  
+               <div class="row">
+                   <div class="col-sm-6 form-group">
+                       <label>Party Name : </label>
+                       <input class="form-control" type="text" name="party_name" value="<?php echo $fetch['party_name']; ?> ">
+                   </div>
+                   <div class="col-sm-6 form-group">
+                       <label>Phone : </label>
+                       <input class="form-control" type="text" name="phone" value="<?php echo $fetch['phone']; ?> ">
+                   </div>
 
-                                           
-                                            <input class="form-control" type="text" name="party_type" value="<?php echo $fetch['party_type']; ?> ">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-6 form-group">
-                                            <label>Phone : </label>
-                                            
-                                            <input class="form-control" type="text" name="phone" value="<?php echo $fetch['phone']; ?> ">
-                                        </div>
-                                        <div class="col-sm-6 form-group">
-                                            <label>Email : </label>
-                                            
-                                            <input class="form-control" type="text" name="email" value="<?php echo $fetch['email']; ?> ">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-6 form-group">
-                                            <label>Address :</label>
-                                            
-                                            
-                                            <textarea class="form-control" name="address_ar" id="" cols="10" rows="2"><?php echo $fetch['address_ar']; ?></textarea>
-                                        </div>
-                                        <div class="col-sm-6 form-group">
-                                            <label>Contact Persone :</label>
-                                            
-                                            <input class="form-control" type="text" name="contact_person" value="<?php echo $fetch['contact_person']; ?> ">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-6 form-group">
-                                            <label>Contact Persone Phone :</label>
-                                            
-                                            <input class="form-control" type="text" name="cp_phone" value="<?php echo $fetch['cp_phone']; ?> ">
-                                        </div>
-                                    </div>
+               </div>
+               <div class="row">
+                   <div class="col-sm-6 form-group">
+                       <label>Email : </label>
+                       <input class="form-control" type="text" name="email" value="<?php echo $fetch['email']; ?> ">
+                   </div>
+                   <div class="col-sm-6 form-group">
+                       <label>Address :</label>
+                       <textarea class="form-control" name="address_ar" id="" cols="10" rows="2"><?php echo $fetch['address_ar']; ?></textarea>
+                   </div>
+               </div>
+               <div class="row">
+                   <div class="col-sm-6 form-group">
+                       <label>Contact Persone :</label>
+                       <input class="form-control" type="text" name="contact_person" value="<?php echo $fetch['contact_person']; ?> ">
+                   </div>
+                   <div class="col-sm-6 form-group">
+                       <label>Contact Persone Phone :</label>
+                       <input class="form-control" type="text" name="cp_phone" value="<?php echo $fetch['cp_phone']; ?> ">
+                   </div>
+               </div>
 
-											<!--form body end -->
-											
-											
-                                          </div>
-                                          <div class="modal-footer">
-<!--                                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>-->
-                                              <button name="submit" class="btn btn-default px-4" type="submit">Update</button>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </form>
+					<!--form body end -->
+				
+            </div>
+                     <div class="modal-footer">
+                         <button name="submit" class="btn btn-default px-4" type="submit">Update</button>
+                     </div>
+            </div>
+        </div>
+    </form>
 
 
 <?php
@@ -105,6 +79,5 @@
 }
 
 ?>
-
 
 <?php include ("./footer.php"); ?>

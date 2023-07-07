4a9821f79a67de2050d698fcbede240ab072662f
shoyab <abumansurshoyab@gmail.com>
2023-07-07 15:48:17 +0600

    Modification in intem form

--- admin/item_info.php
+++ admin/item_info.php
@@ -44,101 +44,113 @@ if (isset($_POST['submit'])){
             <!-- modal button end -->
 
             <!-- modal start -->
-            <div class="modal fade" id="DonorAdd" tabindex="-1" role="dialog" aria-labelledby="DonorAdd" aria-hidden="true">
-                <form action="" method="POST">
-                    <div class="modal-dialog modal-lg" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Item</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
+    <div class="modal fade" id="DonorAdd" tabindex="-1" role="dialog" aria-labelledby="DonorAdd" aria-hidden="true">
+        <form action="" method="POST">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Item</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <!--form body start here-->
+                        <div class="row">
+                            <div class="col-sm-6 form-group">
+                                <label>Item Code : </label>
+                                <input name="icode" class="form-control" type="text" placeholder="Item Code">
                             </div>
-                            <div class="modal-body">
-
-                                <!--form body start here-->
-
-                                <?php
-                                $select = 'SELECT * FROM categories';
-                                $query = $con->query($select);
-                                $result = $query->fetch_all(MYSQLI_ASSOC);
-                                ?>
-
-                                <div class="row">
-                                    <div class="col-sm-6 form-group">
-                                        <label>Item Code : </label>
-                                        <input name="icode" class="form-control" type="text" placeholder="Item Code">
-                                    </div>
-                                    <div class="col-sm-6 form-group">
-                                        <label>Item Name : </label>
-                                        <input name="iname" class="form-control" type="text" placeholder="Item Name">
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-6 form-group">
-                                        <label class="">Select Category</label>
-
-                                        <select name="catid" class="form-control select2_style1" style="width: 100%;">
-                                            <option disabled selected value="0">Select Category</option>
-                                            <?php
-                                                foreach($result as $value): ?>
-                                                    <option value="<?= $value['id']; ?>"><?= $value['cat_name']; ?></option>
-                                                <?php endforeach; ?>
-                                        </select>
-                                    </div>
-                                    <div class="col-sm-6 form-group">
-                                        <label class="">Select Brand</label>
-
-                                        <select name="brand_id" class="form-control select2_style1" style="width: 100%;">
-                                            <option disabled value="0">Select Brand</option>
-                                        <?php 
-                                            $select = 'SELECT * FROM brands';
-                                            $query = $con->query($select);
-                                            $result = $query->fetch_all(MYSQLI_ASSOC);
-                                                foreach($result as $value): ?>
-                                                    <option value="<?= $value['id']; ?>"><?= $value['brand_name']; ?></option>
-                                                <?php endforeach; ?>
-                                          </select>
-                                    </div>
-                                </div>
-                                <div class="row">
-
-                                    <div class="col-sm-6 form-group">
-                                        <label>Unit : </label>
-                                        <input name="unit" class="form-control" type="text" placeholder="Unit">
-                                    </div>
-                                    <div class="col-sm-6 form-group">
-                                        <label>Unit Price :</label>
-                                        <input name="uprice" class="form-control" type="text" placeholder="Unit Price">
-                                    </div>
-
-                                </div>
-                                <div class="row">
-
-                                    <div class="col-sm-6 form-group">
-                                        <label>Last Per Price :</label>
-                                        <input name="lpprice" class="form-control" type="text" placeholder="Last Per Price">
-                                    </div>
-                                </div>
-                                <div class="row">
-                                <div class="col-sm-6 form-group">
-                                        <label>Barcode :</label>
-                                        <input name="barcode" class="form-control" type="text" placeholder="Barcode">
-                                    </div>
-                                </div>
-
-
-                                <!--form body end -->
-
-
-                            </div>
-                            <div class="modal-footer">
-                                <button name="submit" class="btn btn-default px-4" type="submit">Save</button>
+                            <div class="col-sm-6 form-group">
+                                <label>Item Name : </label>
+                                <input name="iname" class="form-control" type="text" placeholder="Item Name">
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="col-sm-6 form-group">
+                                <?php
+                                    $select = 'SELECT * FROM categories';
+                                    $query = $con->query($select);
+                                    $result = $query->fetch_all(MYSQLI_ASSOC);
+                                ?>
+                                <label class="">Select Category</label>
+                                <select name="catid" class="form-control select2_style1" style="width: 100%;">
+                                    <option disabled selected value="0">Select Category</option>
+                                    <?php
+                                        foreach($result as $value): ?>
+                                            <option value="<?= $value['id']; ?>"><?= $value['cat_name']; ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <label class="">Select Brand</label>
+                                <select name="brand_id" class="form-control select2_style1" style="width: 100%;">
+                                <option disabled selected value="0">Select Brand</option>
+                                <?php 
+                                    $select = 'SELECT * FROM brands';
+                                    $query = $con->query($select);
+                                    $result = $query->fetch_all(MYSQLI_ASSOC);
+                                        foreach($result as $value): ?>
+                                            <option value="<?= $value['id']; ?>"><?= $value['brand_name']; ?></option>
+                                        <?php endforeach; ?>
+                                  </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6 form-group">
+                                <label>Unit : </label>
+                                <input name="unit" class="form-control" type="text" placeholder="Unit">
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <label>Unit Price :</label>
+                                <input name="uprice" class="form-control" type="text" placeholder="Unit Price">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6 form-group">
+                                <label>Re Order Level:</label>
+                                <input name="reolevel" class="form-control" type="text" placeholder="Re Order Level">
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <label>Last Per Price :</label>
+                                <input name="lpprice" class="form-control" type="text" placeholder="Last Per Price">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6 form-group">
+                                <label>Barcode :</label>
+                                <input name="barcode" class="form-control" type="text" placeholder="Barcode">
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <label>Status :</label>
+                                <select name="sts" id="" class="form-control" >
+                                    <option value="active">Active</option>
+                                    <option value="inactive">Inactive</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6 form-group">
+                                <label>stock maintain :</label>
+                                <select name="stmaintain" id="" class="form-control" >
+                                    <option value="yes">Yes</option>
+                                    <option value="no">No</option>
+                                </select>
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <label>Opening Stock :</label>
+                                <input name="opstock" class="form-control" type="text" placeholder="Opening Stock">
+                            </div>
+                        </div>
+                        <!--form body end -->
                     </div>
-                </form>
+                    <div class="modal-footer">
+                        <button name="submit" class="btn btn-default px-4" type="submit">Save</button>
+                    </div>
+                </div>
             </div>
+        </form>
+    </div>
             <!-- modal end -->
             <?php
             $select = 'SELECT * FROM item_info';

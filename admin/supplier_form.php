283f46c7722bf619f45b4d8ff10d2753172b4d98
shoyab <abumansurshoyab@gmail.com>
2023-07-07 21:08:19 +0600

    supplier file modified

--- admin/supplier_form.php
+++ admin/supplier_form.php
@@ -4,20 +4,17 @@
 <?php
             if (isset($_POST['submit'])){
                 $pname = $_POST['pname'];
-                $ptype = $_POST['ptype'];
                 $phone = $_POST['phone'];
                 $email = $_POST['email'];
                 $address = $_POST['address'];
                 $conpersone = $_POST['conpersone'];
                 $cpphone = $_POST['cpphone'];
 
-                
-                $insert = "INSERT INTO supplier(party_name,party_type,address_ar,phone,email,contact_person,cp_phone) VALUES ('{$pname}','{$ptype}','{$address}','{$phone}','{$email}','{$conpersone}','{$cpphone}')";
+                $insert = "INSERT INTO `suppliers`(`party_name`, `party_type`, `address_ar`, `phone`, `email`,`contact_person`, `cp_phone`, `user_id`) VALUES ('{$pname}', 'customer', '{$address}', '{$phone}', '{$email}', '{$conpersone}', '{$cpphone}', '0')";
                 $con->query($insert);
 
 };
 ?>
-
     <div class="content-wrapper">
         <!-- START PAGE CONTENT-->
         <div class="page-heading">
@@ -35,17 +32,17 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="ibox">
-        <!-- modal button -->
-        <div class="ibox-head justify-content-end">
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#DonorAdd">
-                Add Supplier
-            </button>
-        </div>
-        <!-- modal button end -->
+                    <!-- modal button -->
+                    <div class="ibox-head justify-content-end">
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#DonorAdd">
+                            Add Supplier
+                        </button>
+                    </div>
+                    <!-- modal button end -->
 
-        <!-- modal start -->
-        <div class="modal fade" id="DonorAdd" tabindex="-1" role="dialog" aria-labelledby="DonorAdd" aria-hidden="true">
+                     <!-- modal start -->
+                            <div class="modal fade" id="DonorAdd" tabindex="-1" role="dialog" aria-labelledby="DonorAdd" aria-hidden="true">
                               <form action="" method="POST">
                                   <div class="modal-dialog modal-lg" role="document">
                                       <div class="modal-content">
@@ -59,39 +56,31 @@
 										  
 										  <!--form body start here-->
 										  
-										  
-										  
                                     <div class="row">
                                         <div class="col-sm-6 form-group">
                                             <label>Party Name : </label>
                                             <input name="pname" class="form-control" type="text" placeholder="Full Name">
                                         </div>
                                         <div class="col-sm-6 form-group">
-                                            <label>Party Type : </label>
-                                            <input name="ptype" class="form-control" type="text" placeholder="Party Type">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-6 form-group">
                                             <label>Phone : </label>
                                             <input name="phone" class="form-control" type="text" placeholder="Phone Number">
                                         </div>
+                                    </div>
+                                    <div class="row">
                                         <div class="col-sm-6 form-group">
                                             <label>Email : </label>
                                             <input name="email" class="form-control" type="email" placeholder="Email">
                                         </div>
-                                    </div>
-                                    <div class="row">
                                         <div class="col-sm-6 form-group">
                                             <label>Address :</label>
                                             <textarea name="address" class="form-control" id="address" cols="10" rows="2" placeholder="Address"></textarea>
                                         </div>
+                                    </div>
+                                    <div class="row">
                                         <div class="col-sm-6 form-group">
                                             <label>Contact Persone :</label>
                                             <input name="conpersone" class="form-control" type="text" placeholder="Contact Persone">
                                         </div>
-                                    </div>
-                                    <div class="row">
                                         <div class="col-sm-6 form-group">
                                             <label>Contact Persone Phone :</label>
                                             <input name="cpphone" class="form-control" type="text" placeholder="Contact Persone Phone">
@@ -100,28 +89,23 @@
 
 											<!--form body end -->
 											
-											
                                           </div>
                                           <div class="modal-footer">
-<!--                                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>-->
                                               <button name="submit" class="btn btn-default px-4" type="submit">Save</button>
                                           </div>
                                       </div>
                                   </div>
                               </form>
-                              
-                          </div>
+                            </div>
         <!-- modal end -->
 
-        <?php 
-            $select = 'SELECT * FROM supplier';
-            $query = $con->query($select);
-            $result = $query->fetch_all(MYSQLI_ASSOC);
-        ?>
+                <?php 
+                    $select = 'SELECT * FROM suppliers';
+                    $query = $con->query($select);
+                    $result = $query->fetch_all(MYSQLI_ASSOC);
+                ?>
 
-
-
-        <div class="ibox-body">
+                    <div class="ibox-body">
                         <div class="table-responsive">
                             <table class="table">
                                 <thead>
@@ -140,30 +124,26 @@
                                 <tbody>
 
                                 <?php foreach($result as $value): ?>
-                            <tr>
-                                <td><?= $value['id']; ?></td>
-                                <td><?= $value['party_name']; ?></td>
-                                <td><?= $value['address_ar']; ?></td>
-                                <td><?= $value['phone']; ?></td>
-                                <td><?= $value['email']; ?></td>
-                                <td><?= $value['contact_person']; ?></td>
-                                <td><?= $value['cp_phone']; ?></td>
+                                <tr>
+                                    <td><?= $value['id']; ?></td>
+                                    <td><?= $value['party_name']; ?></td>
+                                    <td><?= $value['address_ar']; ?></td>
+                                    <td><?= $value['phone']; ?></td>
+                                    <td><?= $value['email']; ?></td>
+                                    <td><?= $value['contact_person']; ?></td>
+                                    <td><?= $value['cp_phone']; ?></td>
 
-                                <td> 
+                                    <td> 
                                     <a href="editsupplier.php?id=<?= $value['id']; ?>" class="btn btn-default btn-xs m-r-5"><i class="fa fa-pencil font-14"></i></a>
 
-
-
-                                 <a href="delete_supplier.php?id=<?= $value['id']; ?>" class="btn btn-default btn-xs m-r-5"><i class="fa fa-trash font-14"></i></a>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-
-            
+                                    <a href="delete_supplier.php?id=<?= $value['id']; ?>" class="btn btn-default btn-xs m-r-5"><i class="fa fa-trash font-14"></i></a>
+                                    </td>
+                                </tr>
+                                <?php endforeach; ?>
                                 </tbody>
                             </table>
                         </div>
-
+                    </div>
 
                     </div>        
                 </div>

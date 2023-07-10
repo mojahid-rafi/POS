acb228840a687dcaf9ea33cfc8b8564666faa10a
Mojahid Rafi <mojahid.rafi17@gmail.com>
2023-07-10 09:40:10 +0600

    General Changes

--- admin/edit_item_info.php
+++ admin/edit_item_info.php
@@ -5,13 +5,13 @@
 <div class="content-wrapper">
     <!-- START PAGE CONTENT-->
     <div class="page-heading">
-<!--        <h1 class="page-title">Edit</h1>-->
-<!--        <ol class="breadcrumb">-->
-<!--            <li class="breadcrumb-item">-->
-<!--                <a href="dashboard.php"><i class="la la-home font-20"></i></a>-->
-<!--            </li>-->
-<!--             <li class="breadcrumb-item"></li>-->
-<!--        </ol>-->
+        <!--        <h1 class="page-title">Edit</h1>-->
+        <!--        <ol class="breadcrumb">-->
+        <!--            <li class="breadcrumb-item">-->
+        <!--                <a href="dashboard.php"><i class="la la-home font-20"></i></a>-->
+        <!--            </li>-->
+        <!--             <li class="breadcrumb-item"></li>-->
+        <!--        </ol>-->
     </div>
 
     <?php
